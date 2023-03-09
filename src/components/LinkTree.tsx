5d67e60b5d397d0f742c5bf407f2948119f10999
Abhinav Gaur <abhinav@spheron.network>
2023-03-09 20:44:55 +0530

    Link Tree App

---
+++ src/components/LinkTree.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import config from '../config';
+
+import RedirectIcon from '../assets/icons/redirect.svg';
+import LogoIcon from '../logo.svg';
+
+import './LinkTree.css';
+
+const LinkTree = () => {
+  return (
+    <div className="linktree-container">
+      <header>
+        <div className="linktree-official-logo">
+          <img src={config.logoUrl} alt="app logo" />
+        </div>
+        <div className="linktree-desc">
+          {config.description}
+        </div>
+      </header>
+      <section className="linktree-main">
+        {config.links.map((link) => {
+          return (
+            <a href={link.redirectUrl} target="_blank" rel="noreferrer" key={link.key}>
+              <div className="linktree-link">
+                <div className="linktree-link-info">
+                  <span className="linktree-link-logo">
+                    <img src={link.iconUrl} alt="link logo" />
+                  </span>
+                  <span className="linktree-link-label">
+                    {link.label}
+                  </span>
+                </div>
+                <span>
+                  <img src={RedirectIcon} alt="external link" />
+                </span>
+              </div>
+            </a>
+          );
+        })}
+      </section>
+      <footer>
+        <div className="linktree-footer">
+          <span className="linktree-powered-text">Powered By: </span>
+          <span className="sph-logo">
+            <img src={LogoIcon} alt="spheron logo" height="25px" />
+          </span>
+        </div>
+      </footer>
+    </div>
+  )
+};
+
+export default LinkTree;
---
+++ src/config.ts
@@ -0,0 +1,61 @@
+import AppLogo from './assets/icons/app-logo.svg';
+import Official from './assets/icons/official.svg';
+import Twitter from './assets/icons/twitter.svg';
+import LinkedIn from './assets/icons/linkedin.svg';
+import Medium from './assets/icons/medium.svg';
+import Youtube from './assets/icons/youtube.svg';
+import Discord from './assets/icons/discord.svg';
+import GitHub from './assets/icons/github.svg';
+
+const config = {
+  "logoUrl": AppLogo,
+  "favicon": "../favicon.ico",
+  "title": "Spheron Link Tree",
+  "description": "Give your dApps, the wings of decentralised infrastructure with Spheron!",
+  "links": [
+    {
+      "key": "official",
+      "label": "Official Landing Page",
+      "iconUrl": Official,
+      "redirectUrl": "https://spheron.network/",
+    },
+    {
+      "key": "twitter",
+      "label": "Twitter",
+      "iconUrl": Twitter,
+      "redirectUrl": "https://twitter.com/SpheronHQ",
+    },
+    {
+      "key": "linkedin",
+      "label": "LinkedIn",
+      "iconUrl": LinkedIn,
+      "redirectUrl": "https://www.linkedin.com/company/spheron/",
+    },
+    // {
+    //   "key": "medium",
+    //   "label": "Medium",
+    //   "iconUrl": Medium,
+    //   "redirectUrl": "",
+    // },
+    {
+      "key": "youtube",
+      "label": "YouTube",
+      "iconUrl": Youtube,
+      "redirectUrl": "https://www.youtube.com/channel/UCIe3FlAWg06kGOrm1-c8oJg",
+    },
+    {
+      "key": "discord",
+      "label": "Discord",
+      "iconUrl": Discord,
+      "redirectUrl": "https://discord.com/invite/ahxuCtm",
+    },
+    {
+      "key": "github",
+      "label": "GitHub",
+      "iconUrl": GitHub,
+      "redirectUrl": "https://github.com/spheronFdn/",
+    }
+  ]
+};
+
+export default config;
