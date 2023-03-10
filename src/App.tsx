ef0fa47e53c9c214ab2b6f37b64e1b301cf2b2ac
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2023-03-10 19:14:17 +0530

    Update footer text and fix formatting and public icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import LinkTree from './components/LinkTree';
+import LinkTree from "./components/LinkTree";
 
-import './App.css';
-import config from './config';
+import "./App.css";
+import config from "./config";
 
 function App() {
   return (
@@ -12,10 +12,7 @@ function App() {
       <Helmet>
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href={config.favicon} />
-        <meta
-          name="description"
-          content={config.description}
-        />
+        <meta name="description" content={config.description} />
         <meta property="og:title" content={config.title} />
         <meta property="og:description" content={config.description} />
         <title>Spheron Link Tree</title>
--- src/components/LinkTree.tsx
+++ src/components/LinkTree.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import config from '../config';
+import config from "../config";
 
-import RedirectIcon from '../assets/icons/redirect.svg';
-import LogoIcon from '../logo.svg';
+import RedirectIcon from "../assets/icons/redirect.svg";
+import LogoIcon from "../logo.svg";
 
-import './LinkTree.css';
+import "./LinkTree.css";
 
 const LinkTree = () => {
   return (
@@ -14,22 +14,23 @@ const LinkTree = () => {
         <div className="linktree-official-logo">
           <img src={config.logoUrl} alt="app logo" />
         </div>
-        <div className="linktree-desc">
-          {config.description}
-        </div>
+        <div className="linktree-desc">{config.description}</div>
       </header>
       <section className="linktree-main">
         {config.links.map((link) => {
           return (
-            <a href={link.redirectUrl} target="_blank" rel="noreferrer" key={link.key}>
+            <a
+              href={link.redirectUrl}
+              target="_blank"
+              rel="noreferrer"
+              key={link.key}
+            >
               <div className="linktree-link">
                 <div className="linktree-link-info">
                   <span className="linktree-link-logo">
                     <img src={link.iconUrl} alt="link logo" />
                   </span>
-                  <span className="linktree-link-label">
-                    {link.label}
-                  </span>
+                  <span className="linktree-link-label">{link.label}</span>
                 </div>
                 <span>
                   <img src={RedirectIcon} alt="external link" />
@@ -41,14 +42,14 @@ const LinkTree = () => {
       </section>
       <footer>
         <div className="linktree-footer">
-          <span className="linktree-powered-text">Powered By: </span>
+          <span className="linktree-powered-text">Powered By </span>
           <span className="sph-logo">
             <img src={LogoIcon} alt="spheron logo" height="25px" />
           </span>
         </div>
       </footer>
     </div>
-  )
+  );
 };
 
 export default LinkTree;
